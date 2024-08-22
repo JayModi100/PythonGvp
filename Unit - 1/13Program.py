d475b218fff245d83efec99d39e0ccba67b672b4
JayModi100 <jayheruwala100@gmail.com>
2024-08-22 10:00:09 +0000

    Unit1Complete

---
+++ Unit - 1/13Program.py
@@ -0,0 +1,17 @@
+
+price = int(input("Enter the Price : "));
+notes = [1,5,10]
+
+if price < min(notes):
+    print("Your Value is less Then ")
+else:
+    # notes.sort(reverse=True)
+    notes.reverse()
+    # print(notes)   
+    print(f"{notes[0]}  {notes[1]}  {notes[2]}")
+ 
+    for f1 in range((price // notes[0]) + 1):
+        for f2 in range((price // notes[1]) + 1):
+            for f3 in range((price // notes[2])+1):
+                if f1*notes[0] + f2*notes[1] + f3*notes[2] == price:
+                    print(f"{f1}  {f2}  {f3}")
