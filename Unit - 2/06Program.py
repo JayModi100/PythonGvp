22bb13cf2a72c65d9456aaef027a6ed9b19d5e84
JayModi100 <jayheruwala100@gmail.com>
2024-09-11 16:46:48 +0530

    Update 06Program.py

--- Unit - 2/06Program.py
+++ Unit - 2/06Program.py
@@ -2,6 +2,19 @@ import Module06 as Matrix
 
 matrix1 = Matrix.initializeMatrix(3,3,[1,2,3,4,5,6,7,8,9])
 matrix2 = Matrix.initializeMatrix(3,3,[1,2,3,4,5,6,7,8,9])
-print(matrix1)
+
+
+print("Matrix - 1")
 Matrix.printMatrix(matrix1)
+print("\nMatrix - 2")
 Matrix.printMatrix(matrix2)
+
+addition = Matrix.additionOfMatrix(matrix1,matrix2)
+print("\nAddition of 2 Matrix : ")
+Matrix.printMatrix(addition)
+
+multiplication = Matrix.multiplicationOfMatrix(matrix1,matrix2)
+print("Multiplication : ")
+Matrix.printMatrix(multiplication)
+
+
