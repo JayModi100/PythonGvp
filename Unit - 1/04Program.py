c075807aaef328cc91487eaa5c735b8cbd288465
JayModi100 <jayheruwala100@gmail.com>
2024-09-15 09:31:01 +0530

    Update 04Program.py

--- Unit - 1/04Program.py
+++ Unit - 1/04Program.py
@@ -1,20 +1,18 @@
 # Word shuffle using string manipulation
 
-WordStr = "Hello I am One Line String For Suffle "
-# WordStr = str(input("Enter the String : "))
+import random
 
+string  = "Hii, Hello I ma Jay How Wans Going What you doing"
 
-list = WordStr.split(" ")
-list1 = ""
-mid = len(list)//2
-
-for i in range(mid):
-    list1+=list[i]
-    list1+=" "
-    list1+=list[mid+i]
-    list1+=" " 
-    
-print(list1)
+words = string.split(" ")
+list = []
+for i in range(len(words)):
+    while True:
+        x = random.randint(0,len(words)-1)
+        if x not in list:
+            list.append(x)
+            break
+    print(words[x],end=" ")
 
 
 
