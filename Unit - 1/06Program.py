07fb3bc2554c56bde8f6cc874a5bad4d8d8ca524
JayModi100 <jayheruwala100@gmail.com>
2024-09-15 09:53:43 +0530

    Update 06Program.py

--- Unit - 1/06Program.py
+++ Unit - 1/06Program.py
@@ -1,18 +1,21 @@
-# Find the student from cs department where the roll no may be in capital or small case letter
+student_info = input(" Enter 2 Character of Your Branch \n Enter 2 digit of year \n Enter 1 Character of your Degree \n Enter Roll No \n Ex : CS20M15 \n Enter Your details : ")
 
-while 1:
-    student_detail = input("Enter Your Department and Roll number[Department-RollNo] : ").upper();
-    print(student_detail)
-    if '-' not in student_detail:
-        print("Use Format please [Department-RollNo] ")
-        continue
-    else:
-        break
+branch = student_info[0:2].upper()
+year = student_info[2:4]
+degree = student_info[4:5].upper()
+roll_no = student_info[5:len(student_info)]
 
-department , roll_no = student_detail.split('-')
-
-if(department == "CS"):
-    print("Student is Belong's to Computer Science department")
-else:
-    print(f"student is Belongs to {department} department")
-
+if branch == "CS":
+    branch = "Computer Science"
+    
+    if degree == "M":
+        degree = "MCA"
+    elif degree == "B":
+        degree = "BCA"
+    elif degree == "P":
+        degree = "PGDCA"
+    
+    print("Student is from Computer Science Department")
+    print(f"Branch = {branch}, Year = 20{year}, degree = {degree}, Roll No = {roll_no}")
+    
+        
