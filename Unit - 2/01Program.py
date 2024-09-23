7c636964df355730bb1bbe0cb5be2e04e460ca61
JayModi100 <jayheruwala100@gmail.com>
2024-09-23 11:40:33 +0530

    Final Program
    This is a last change of this

--- Unit - 2/01Program.py
+++ Unit - 2/01Program.py
@@ -1,12 +1,46 @@
-number = int(input("Enter the number : "))
-sum=0
+def checkPerfectNumber(number):
+    sum,list = 0,[]
+    for i in range(1,(number // 2)+1):
+        if number % i == 0:
+            list.append(i)
+            sum+=i
+    return sum,list
+    
+    
+inputNumber = int(input("Enter the number to check number is perfect or not : "))
+result,resultList = checkPerfectNumber(inputNumber)
 
-for i in range(1,(number//2)+1):
-    if(number % i == 0): 
-        sum+=i
+if result == inputNumber:
+    print(f"{result} is perfect number")
+    print(f"{result} = ",end=" ")
+    
+    lenthOfList = len(resultList)
+    
+    for i in range(lenthOfList):
+        print(f"{resultList[i]}",end="")
         
-
-if(sum == number):
-    print(f"{number} is perfect no It's {sum}")
+        if (i) != lenthOfList-1:
+            print(" + ",end="")  
 else:
-    print(f"{number} is not perfect no {sum}")
+    print(f"{inputNumber} is not a perfect Number")    
+    
+print()
+print("Print Perferct Number Between 1 to 10000")
+
+
+for number in range(1,10000):
+    result,resultList = checkPerfectNumber(number)
+
+    if result == number:
+        print(f"{result} is perfect number")
+        print(f"{result} = ",end=" ")
+        
+        lenthOfList = len(resultList)
+        
+        for i in range(lenthOfList):
+            print(f"{resultList[i]}",end="")
+            
+            if (i) != lenthOfList-1:
+                print(" + ",end="") 
+        
+        print("")
